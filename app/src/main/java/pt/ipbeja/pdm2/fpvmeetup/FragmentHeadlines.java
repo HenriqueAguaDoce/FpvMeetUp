58e6ff2ba35b701a6ebcf78eefa79dac95caee08
HenriqueAD <henriqueaguadoce@gmail.com>
2017-01-08 19:50:02 +0000

    Reimplementação da aplicação "News" - Ponto 3

---
+++ app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/FragmentHeadlines.java
@@ -0,0 +1,30 @@
+package pt.ipbeja.pdm2.fpvmeetup;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragmentHeadlines extends Fragment {
+
+
+    public FragmentHeadlines() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.headlines);
+        return textView;
+    }
+
+}
