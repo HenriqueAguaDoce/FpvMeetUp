a862a1c6a1f4b4f35951e0c04ee119113ffb7059
HenriqueAD <henriqueaguadoce@gmail.com>
2017-01-08 19:58:47 +0000

    Reimplementação da aplicação "News" - Ponto 4

--- app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/MainActivity.java
@@ -2,6 +2,7 @@ package pt.ipbeja.pdm2.fpvmeetup;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toast.makeText(this, "OnCreate", Toast.LENGTH_SHORT).show();
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            FragmentHeadlines head = new FragmentHeadlines();
+            head.setArguments(getIntent().getExtras());
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, head).commit();
+        }
+
     }
 }
