c204e0aceb59e6fbcf5d55d5f169ecc5b193cc64
HenriqueAD <henriqueaguadoce@gmail.com>
2017-01-09 19:46:04 +0000

    Reimplementação da aplicação "News" - Ponto 10

--- app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/FragmentArticle.java
+++ app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/FragmentArticle.java
@@ -12,7 +12,7 @@ import android.widget.TextView;
  * A simple {@link Fragment} subclass.
  */
 public class FragmentArticle extends Fragment {
-
+    int currentPosition;
 
     public FragmentArticle() {
         // Required empty public constructor
@@ -24,6 +24,16 @@ public class FragmentArticle extends Fragment {
                              Bundle savedInstanceState) {
 
         return inflater.inflate(R.layout.fragment_article, container, false);
+    }
+
+    @Override public void onStart() {
+        super.onStart();
+        Bundle args = getArguments();
+        if (args != null) {
+            currentPosition = args.getInt("position");
+            TextView article = (TextView) getActivity().findViewById(R.id.txtNote);
+            article.setText(NewData.Articles[currentPosition]);
+        }
     }
 
 }
