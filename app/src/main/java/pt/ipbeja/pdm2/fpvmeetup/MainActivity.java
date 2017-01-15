f9b184ecfbe0072f66872222ba8e3400f1f4e83a
HenriqueAD <henriqueaguadoce@gmail.com>
2017-01-15 02:48:20 +0000

    The user can now see his location on GoogleMaps

--- app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/MainActivity.java
+++ app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/MainActivity.java
@@ -1,13 +1,22 @@
 package pt.ipbeja.pdm2.fpvmeetup;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements LocationListener {
+    Location location;
     private boolean checkBack = false;
 
     @Override
@@ -15,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        String bestProvider = locationManager.getBestProvider(criteria,true);
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        location = locationManager.getLastKnownLocation(bestProvider);
+        if (location == null) {
+            onLocationChanged(location);
+        }
+        locationManager.requestLocationUpdates(bestProvider,20000,0,this);
     }
 
     @Override
@@ -46,5 +68,34 @@ public class MainActivity extends AppCompatActivity {
     public void imgBuild_onClick(View view) {
         Intent i = new Intent(this, BuildInfo.class);
         startActivity(i);
+    }
+
+    public void imgLocation_onClick(View view) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
+
+        Uri uri = Uri.parse("geo:" + latitude + "," + longitude);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
     }
 }
