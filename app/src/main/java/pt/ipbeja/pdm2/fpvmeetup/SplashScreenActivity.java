cdbe753a0c8415324d5946ab17357bd32610196a
HenriqueAD <henriqueaguadoce@gmail.com>
2017-01-16 23:15:02 +0000

    Added animation to my SplashScreen

--- app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/SplashScreenActivity.java
+++ app/src/main/java/pt/ipbeja/pdm2/fpvmeetup/SplashScreenActivity.java
@@ -1,9 +1,12 @@
 package pt.ipbeja.pdm2.fpvmeetup;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
 import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SplashScreenActivity extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 3000;
@@ -14,6 +17,13 @@ public class SplashScreenActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash_screen);
 
         getSupportActionBar().setTitle("");
+
+        View image = findViewById(R.id.splashScreen);
+        Animator anim = AnimatorInflater
+                .loadAnimator(this, R.animator.flip_on_vertical);
+        anim.setTarget(image);
+        anim.setDuration(2000L);
+        anim.start();
 
         new Handler().postDelayed(new Runnable() {
             @Override
